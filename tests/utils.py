f637ed8d2a6f2bcb3b81b02fc3b515abb8904fd6
John Vandenberg <jayvdb@gmail.com>
2015-04-14 11:43:40 +1000

    More Win32 os.environ issues
    Change-Id: I107bf72209d3d6f1ae62ecad4c3e8c989160ae05

--- tests/utils.py
+++ tests/utils.py
@@ -260,13 +260,13 @@ def execute(command, data_in=None, timeout=0, error=None):
     # str() on Python 2.
     env = os.environ.copy()
     # sys.path may have been modified by the test runner to load dependencies.
-    env['PYTHONPATH'] = ":".join(sys.path)
+    env[str('PYTHONPATH')] = str(":".join(sys.path))
     # LC_ALL is used by i18n.input as an alternative for userinterface_lang
     if pywikibot.config.userinterface_lang:
-        env['LC_ALL'] = str(pywikibot.config.userinterface_lang)
+        env[str('LC_ALL')] = str(pywikibot.config.userinterface_lang)
     # Set EDITOR to an executable that ignores all arguments and does nothing.
     if sys.platform == 'win32':
-        env['EDITOR'] = str('call')
+        env[str('EDITOR')] = str('call')
     else:
         env['EDITOR'] = 'true'
     options = {
