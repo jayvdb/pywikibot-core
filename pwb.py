e3c9616379fb2281c52c63b8accde5a31ab073e7
John Vandenberg <jayvdb@gmail.com>
2016-05-26 17:29:12 +0700

    Add PyPy builds to Jenkins and Travis
    - unicodedata2 and lunatic-python are not PyPy compatible
    
    Bug: T101592
    Change-Id: Ide8bcef49499d7f3bb7bcbf00bde3d42936e5672

--- pwb.py
+++ pwb.py
@@ -9,7 +9,7 @@ Run scripts using:
 and it will use the package directory to store all user files, will fix up
 search paths so the package does not need to be installed, etc.
 """
-# (C) Pywikibot team, 2015
+# (C) Pywikibot team, 2016
 #
 # Distributed under the terms of the MIT license.
 #
@@ -32,6 +32,12 @@ from warnings import warn
 PYTHON_VERSION = sys.version_info[:3]
 PY2 = (PYTHON_VERSION[0] == 2)
 PY26 = (PYTHON_VERSION < (2, 7))
+
+try:
+    sys.pypy_version_info
+    PYPY = True
+except AttributeError:
+    PYPY = False
 
 versions_required_message = """
 Pywikibot not available on:
@@ -147,9 +153,15 @@ def abspath(path):
 absolute_path = abspath(os.path.dirname(sys.argv[0]))
 rewrite_path = absolute_path
 
-sys.path = [sys.path[0], rewrite_path,
-            os.path.join(rewrite_path, 'pywikibot', 'compat'),
-            ] + sys.path[1:]
+pywikibot_compat_dirpath = os.path.join(rewrite_path, 'pywikibot', 'compat')
+
+if PYPY:
+    # Workaround https://bitbucket.org/pypy/pypy/issues/2314
+    sys.path += [rewrite_path, pywikibot_compat_dirpath]
+else:
+    sys.path = [sys.path[0], rewrite_path,
+                os.path.join(rewrite_path, 'pywikibot', 'compat'),
+                ] + sys.path[1:]
 
 try:
     import requests
--- tests/pwb_tests.py
+++ tests/pwb_tests.py
@@ -21,6 +21,12 @@ from tests import join_tests_path, create_path_func
 from tests.utils import execute, execute_pwb
 from tests.aspects import unittest, PwbTestCase
 
+try:
+    sys.pypy_version_info
+    PYPY = True
+except AttributeError:
+    PYPY = False
+
 join_pwb_tests_path = create_path_func(join_tests_path, 'pwb')
 
 
@@ -44,7 +50,11 @@ class TestPwb(PwbTestCase):
         script_path = join_pwb_tests_path(name + '.py')
 
         direct = execute([sys.executable, '-m', package_name])
+        self.assertNotIn('RuntimeError', direct['stderr'])
+
         vpwb = execute_pwb([script_path])
+        self.assertNotIn('RuntimeError', vpwb['stderr'])
+
         self.maxDiff = None
         self.assertEqual(direct['stdout'], vpwb['stdout'])
 
@@ -57,6 +67,8 @@ class TestPwb(PwbTestCase):
         Make sure the environment is not contaminated, and is the same as
         the environment we get when directly running a script.
         """
+        if PYPY:
+            raise unittest.SkipTest('Fails on PyPy, dumping secure env vars')
         self._do_check('print_env')
 
     def test_locals(self):
